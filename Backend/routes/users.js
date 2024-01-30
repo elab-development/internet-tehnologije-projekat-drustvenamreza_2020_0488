6802c933a2afddd8ff32c919f6d543f0be522ca8
KrksiA <andrejkrkic46@gmail.com>
2024-01-30 21:36:23 +0100

    Delete i Get za korisnike

--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -4,7 +4,7 @@ const User = require("../models/User");
 
 //update
 router.put("/:id", async(req, res)=>{
-    if(req.body.userId === req.params.id || req.user.isAdmin){
+    if(req.body.userId === req.params.id || req.body.isAdmin){
 
         if(req.body.password){
             try{
@@ -30,8 +30,37 @@ router.put("/:id", async(req, res)=>{
         return res.status(403).json("Mozes da update-ujes samo svoj nalog!")
     }
 })
+
 //delete
+router.delete("/:id", async(req, res)=>{
+    if(req.body.userId === req.params.id || req.body.isAdmin){
+
+        try{
+            const user = await User.findByIdAndDelete(req.params.id)
+            res.status(200).json("Account je obrisan")
+
+        }catch(err){
+            return res.status(500).json(err + " greskaaa")
+        }
+
+    }else{
+        return res.status(403).json("Mozes da delete-ujes samo svoj nalog!")
+    }
+})
 //get user
+router.get("/:id", async(req, res)=>{
+try{
+
+    const user = await User.findById(req.params.id)
+    const {password, isAdmin, updatedAt, ...other} = user._doc //pravi dokument i u njega smesta objekat koji sadrzi sve ove propertije
+    res.status(200).json(other) //saljemo mu samo other (sve bez napisanih properyija)
+}catch(err){
+    return res.status(500).json(err + " greskaaa")
+}
+
+});
+
+
 //follow
 //unfollow
 
