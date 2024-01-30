3b2efe77fbfb15d5993ef20280be06c9ed8653cb
KrksiA <andrejkrkic46@gmail.com>
2024-01-30 17:08:45 +0100

    Password Hash
    Dodata enkripcija sifre za korisnike u bazi.

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -1,19 +1,28 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const bcrypt = require("bcrypt");
 
 router.post('/register', async (req, res)=>{
 
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
+   
 
 try{
 
+    //generate new password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+
+    //create new user
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: hashedPassword
+    })
+
+//save user and return response
     const user = await newUser.save();
     res.status(200).json(user);
-}catch(err){
+} catch(err){
 console.log(err)
 }
 
--- Backend/routes/users.js
+++ Backend/routes/users.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 
 
 router.get('/', (req, res)=>{
-res.send("hey its user rout");
+res.send("hey its user route");
 })
 
 module.exports = router
