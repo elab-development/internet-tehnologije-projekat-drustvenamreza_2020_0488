3ac15af3e5f6a9b129edad28ca9c7fd564269395
teodoraboskovic <boskovic.tea73@gmail.com>
2024-01-31 13:15:20 +0100

    Top menubar

---
+++ Frontend/myapp/src/App.js
@@ -0,0 +1,10 @@
+import Home from "./pages/home/home";
+//import {Person} from "@mui/icons-material"//import ikonice osobe
+
+function App() {
+  return (
+    //pozivanje pocetne stranice
+   <Home/>
+  );
+}
+export default App;
