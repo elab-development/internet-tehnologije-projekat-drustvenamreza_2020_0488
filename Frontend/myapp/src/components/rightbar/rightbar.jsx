e1d87d89e32cb9d16c0775b7a3a80ccbfb74aa17
teodoraboskovic <boskovic.tea73@gmail.com>
2024-02-01 01:50:27 +0100

    home page right section

--- Frontend/myapp/src/components/rightbar/rightbar.jsx
+++ Frontend/myapp/src/components/rightbar/rightbar.jsx
@@ -1,8 +1,54 @@
 import "./rightbar.css"
-export default function Rightbar(){
-    return(
+export default function Rightbar() {
+    return (
         <div className="rightBar">
-            rightbar
+            <div className="rightbarWrapp">
+                <div className="birthdayNotification">
+                    <img src="/assets/gift3.png" alt="" className="birthdayImage"/>
+                    <span className="birthdayText">
+                        <b>+2 ljudi</b> je rodjeno istog dana
+                    </span>
+                </div>
+                <img src="/assets/ad.png" alt=""className="AdImage" />
+                <h4 className="rightbarTitle">Online prijatelji</h4>
+                <ui className="rightbarOnlineFriendList">
+                    <li className="rightbarFriend">
+                        <div className="profileIconOnlineFriend">
+                            <img src="/assets/profilePictures/4.jpeg" alt="" className="profilePictureOnlineFriend"/>
+                            <span className="onlineFriendBadge"></span>
+                        </div>
+                        <span className="rightbarOnlineFriendUsername">Lena Markovic</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="profileIconOnlineFriend">
+                            <img src="/assets/profilePictures/4.jpeg" alt="" className="profilePictureOnlineFriend"/>
+                            <span className="onlineFriendBadge"></span>
+                        </div>
+                        <span className="rightbarOnlineFriendUsername">Lena Markovic</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="profileIconOnlineFriend">
+                            <img src="/assets/profilePictures/4.jpeg" alt="" className="profilePictureOnlineFriend"/>
+                            <span className="onlineFriendBadge"></span>
+                        </div>
+                        <span className="rightbarOnlineFriendUsername">Lena Markovic</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="profileIconOnlineFriend">
+                            <img src="/assets/profilePictures/4.jpeg" alt="" className="profilePictureOnlineFriend"/>
+                            <span className="onlineFriendBadge"></span>
+                        </div>
+                        <span className="rightbarOnlineFriendUsername">Lena Markovic</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="profileIconOnlineFriend">
+                            <img src="/assets/profilePictures/4.jpeg" alt="" className="profilePictureOnlineFriend"/>
+                            <span className="onlineFriendBadge"></span>
+                        </div>
+                        <span className="rightbarOnlineFriendUsername">Lena Markovic</span>
+                    </li>
+                </ui>
+            </div>
         </div>
     )
 }
